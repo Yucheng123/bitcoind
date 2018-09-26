a9452e8ddaa52ca66bc3f50c2cd1b4b6f7524064
LAPTOP-8TNHDKJ8\wuwen <407094141@qq.com>
2018-09-26 16:07:12 +0800

    测试

--- src/main/java/com/_37coins/bcJsonRpc/pojo/NeoTransaction.java
+++ src/main/java/com/_37coins/bcJsonRpc/pojo/NeoTransaction.java
@@ -17,44 +17,4 @@ public class NeoTransaction {
     private Long nonce;
     private List<Vin> vin;
     private List<NeoVout> vout;
-
-    public String getTxid() {
-        return txid;
-    }
-
-    public void setTxid(String txid) {
-        this.txid = txid;
-    }
-
-    public Long getSize() {
-        return size;
-    }
-
-    public void setSize(Long size) {
-        this.size = size;
-    }
-
-    public Long getNonce() {
-        return nonce;
-    }
-
-    public void setNonce(Long nonce) {
-        this.nonce = nonce;
-    }
-
-    public List<Vin> getVin() {
-        return vin;
-    }
-
-    public void setVin(List<Vin> vin) {
-        this.vin = vin;
-    }
-
-    public List<NeoVout> getVout() {
-        return vout;
-    }
-
-    public void setVout(List<NeoVout> vout) {
-        this.vout = vout;
-    }
 }
